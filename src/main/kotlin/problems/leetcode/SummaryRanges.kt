22d74f8771e0fdf796e195707cf968b555e45267
Nandra Saputra <nandra.saputras@gmail.com>
2023-11-01 15:54:34 +0700

    add SummaryRanges

---
+++ src/main/kotlin/problems/leetcode/SummaryRanges.kt
@@ -0,0 +1,32 @@
+package problems.leetcode
+
+// https://leetcode.com/problems/summary-ranges/?envType=study-plan-v2&envId=top-interview-150
+object SummaryRanges {
+    fun summaryRanges(nums: IntArray): List<String> {
+        val result = mutableListOf<String>()
+        var currentIndex = 0
+        while (currentIndex < nums.size) {
+            val currentResult = mutableSetOf<Int>()
+            for (innerIndex in currentIndex until nums.size) {
+                val currentValue = nums[innerIndex]
+                currentResult.add(currentValue)
+                if (innerIndex + 1 == nums.size || currentValue + 1 != nums[innerIndex + 1]) {
+                    currentIndex = innerIndex + 1
+                    break
+                }
+            }
+            result.apply {
+                if (currentResult.size == 1) {
+                    add("${currentResult.first()}")
+                } else {
+                    add("${currentResult.first()}->${currentResult.last()}")
+                }
+            }
+        }
+        return result
+    }
+}
+
+fun main() {
+    print( SummaryRanges.summaryRanges(intArrayOf(0,2,3,4,6,8,9)))
+}
